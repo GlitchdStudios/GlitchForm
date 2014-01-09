c04b925e8e79bd3054e988c4b9ef9d893e97277a
Soundwave <Soundwave@.(none)>
2014-01-08 22:09:05 -0500

    Added an Arc to the outershield effect.

--- Assets/Standard Assets/Scripts/Enemies/Bosses/Shield Drones/ShieldEmission.cs
+++ Assets/Standard Assets/Scripts/Enemies/Bosses/Shield Drones/ShieldEmission.cs
@@ -4,35 +4,36 @@ using System.Collections;
 public class ShieldEmission : MonoBehaviour
 {
 	public GameObject targetObj;
+	public GameObject shieldNode;
 
 	private LineRenderer lineRef;
 	private Transform thisTransform;
 	private int points;
+	private float t;
 
 	// Use this for initialization
 	void Start ()
 	{
+		points = 10;
 		lineRef = GetComponent<LineRenderer>();
+		lineRef.SetVertexCount(points);
 		thisTransform = transform;
-		points = 4;
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		lineRef.SetPosition(0, thisTransform.position);
-
-		for(int i = 1; i < points; i++)
+		for(int i = 0; i < points; i++)
 		{
-			Vector2 pos = Vector2.Lerp(thisTransform.position,targetObj.transform.position, i/4.0f);
+			//Vector2 pos = Vector2.Lerp(thisTransform.position,targetObj.transform.position, i/4.0f);
+			t = i/(float)(points-1);
+			Vector2 arc = Mathf.Pow(1-t,2)*thisTransform.position + 2*t*(1-t)*shieldNode.transform.position + Mathf.Pow(t,2)*targetObj.transform.position;
 
-			pos.x += Random.Range(-0.4f,0.4f);
-			pos.y += Random.Range(-0.4f,0.4f);
+			arc.x += Random.Range(-0.2f,0.2f);
+			arc.y += Random.Range(-0.2f,0.2f);
 
-			lineRef.SetPosition(i, pos);
+			lineRef.SetPosition(i, arc);
 		}
-		
-		lineRef.SetPosition(points, targetObj.transform.position);
 	}
 }
 
