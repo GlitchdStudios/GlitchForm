f5118876fbd1311d7fcd0474070b43fd037506ac
Soundwave <Soundwave@.(none)>
2014-01-10 20:50:41 -0500

    Shield Wave and Shield Emission effects have been added to the Shield Drone implementation.

--- Assets/Standard Assets/Scripts/Enemies/Bosses/Shield Drones/ShieldEmission.cs
+++ Assets/Standard Assets/Scripts/Enemies/Bosses/Shield Drones/ShieldEmission.cs
@@ -14,7 +14,7 @@ public class ShieldEmission : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		points = 10;
+		points = 20;
 		lineRef = GetComponent<LineRenderer>();
 		lineRef.SetVertexCount(points);
 		thisTransform = transform;
@@ -25,13 +25,12 @@ public class ShieldEmission : MonoBehaviour
 	{
 		for(int i = 0; i < points; i++)
 		{
-			//Vector2 pos = Vector2.Lerp(thisTransform.position,targetObj.transform.position, i/4.0f);
 			t = i/(float)(points-1);
 			Vector2 arc = Mathf.Pow(1-t,2)*thisTransform.position + 2*t*(1-t)*shieldNode.transform.position + Mathf.Pow(t,2)*targetObj.transform.position;
-
-			arc.x += Random.Range(-0.2f,0.2f);
-			arc.y += Random.Range(-0.2f,0.2f);
-
+			
+			arc.x += Random.Range(-0.1f,0.1f);
+			arc.y += Random.Range(-0.1f,0.1f);
+			
 			lineRef.SetPosition(i, arc);
 		}
 	}
