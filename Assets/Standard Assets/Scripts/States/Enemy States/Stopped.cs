470a8fe75d2b19890f80ef9f0d9e21209013ce23
Soundwave <Soundwave@.(none)>
2014-02-02 19:08:47 -0500

    Smooth out drone movment when attempting to move towards the player.

--- Assets/Standard Assets/Scripts/States/Enemy States/Stopped.cs
+++ Assets/Standard Assets/Scripts/States/Enemy States/Stopped.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class Stopped : AbstractState
 {
 	private float m_speed;
+	private float time;
+	private float timer;
 
 	// Use this for initialization
 	void Start ()
 	{
-		m_speed = 0f;
+		m_speed = 0.01f;
+		timer = 2.0f;
 	}
 
 	public override void ResolveState(BaseEntity baseEntity)
@@ -18,7 +21,12 @@ public class Stopped : AbstractState
 
 	public void ChangeSpeed(BaseEntity baseEntity)
 	{
-		baseEntity.baseSpeed = m_speed;
+		time = Time.time + timer;
+
+		if(time >= Time.time)
+		{
+			baseEntity.baseSpeed -= m_speed;
+		}
 	}
 }
 
